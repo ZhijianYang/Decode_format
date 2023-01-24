3af0f5cc95e5d60b40707ef4584ecfb0b53fa738
ZhijianYang <67515690+ZhijianYang@users.noreply.github.com>
2023-01-24 14:57:58 +0100

    Add files via upload

---
+++ check_format_decode.R
@@ -0,0 +1,194 @@
+##
+
+library(data.table)
+
+# check size of the origin files
+filelist <- list.files('/NiuHe/decode.is/',pattern = 'txt.gz')
+length(filelist)
+
+table(fls > 940000000) # TRUE 4907
+
+################################################################
+
+setwd('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/')
+filelist = list.files('SNP_list/',pattern = '.rds')
+
+for (i in 1:length(filelist)) {
+	b <- readRDS(paste0('SNP_list/',filelist[i]))
+
+	cat(sub(filelist[i],pattern = '.rds',replacement = ''),'\n')
+
+	if (nrow(b) == 10293879) {
+		cat('rownum: ',nrow(b),', checked. \n')
+	} else {
+		cat('rownum: ',nrow(b),', check failed. \n')
+	}
+
+	if (i == 1) {
+		SNP_list <- b$rsids
+	} else {
+		cat('All rsids matched',all(SNP_list == b$rsids),'\n')
+	}
+
+
+	cat('All effectAlleles matched.',all(a$effectAllele == b$effectAllele),'\n')
+	cat('All otherAlleles matched.',all(a$otherAllele == b$otherAllele),'\n')
+	
+	cat('\n')
+
+}
+
+saveRDS(b,'SNP_list.rds')
+
+################################################################
+
+library(data.table)
+
+setwd('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/')
+
+write.line <- function(x, file, append = TRUE) {
+  write.table(x, file, row.names = FALSE, col.names = FALSE, quote = FALSE, append = append)
+}
+
+write.line(x = 'protein that rownum does not equal to 10293879',file = 'logs/rownum_fails.txt',append = FALSE)
+write.line(x = 'protein that rsids, effectAllele, otherAllele are not matched',file = 'logs/rsids_fails.txt',append = FALSE)
+
+sink('logs/check_format_decode.log',split = TRUE)
+
+## The 10293879 SNPs list
+b <- readRDS('SNP_list.rds')
+
+filelist <- list.files('sumstat/',pattern = 'txt.gz')
+
+for (i in 1:length(filelist)) {
+
+	protein <- sub(filelist[i],pattern='.txt.gz',replacement = '')
+	cat(i,'\t',protein,'\n')
+
+	a <- fread(paste0('sumstat/',filelist[i]))
+	if (nrow(a) == 10293879) {
+		cat('rownum: ',nrow(a),', checked. \n')
+	} else {
+		cat('rownum: ',nrow(a),', check failed. \n')
+		write.line(x = protein,file = 'rownum_fails.txt')
+	}
+	
+	if (all(a$rsids == b$rsids) & all(a$effectAllele == b$effectAllele) & all(a$otherAllele == b$otherAllele)) {
+		cat('rsids,effectAllele,otherAllele are matched. \n')
+	} else {
+		cat('rsids,effectAllele,otherAllele are not matched. \n')
+		write.line(x = protein,file = 'rsids_fails.txt')
+	}
+	cat('\n')
+}
+
+sink()
+
+################################################################
+
+# shell 
+
+# zhijian@LargeBlack:/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/logs$ grep 'rsids,effectAllele,otherAllele are matched' check_format_decode.log| wc -l
+# 1881
+
+# zhijian@LargeBlack:/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/logs$ grep 'checked' check_format_decode.log| wc -l
+# 1881
+
+################################################################
+
+
+library(data.table)
+
+setwd('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/')
+
+filelist <- list.files('sumstat/',pattern = 'txt.gz')
+
+if (!dir.exists("/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/B_SE_N/")){dir.create("/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/B_SE_N/")}
+
+for (i in 1:length(filelist)) {
+
+	print(Sys.time())
+	protein <- sub(filelist[i],pattern='.txt.gz',replacement = '')
+	cat(i,'\t',protein,'\n')
+
+	if (file.exists(paste0('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/B_SE_N/',protein,'.txt.gz'))) {
+		cat('Result file exist, next \n')
+		next
+	}
+
+	a <- fread(paste0('sumstat/',filelist[i]),data.table = FALSE)
+
+	b <- a[,c('Beta','SE','N')]
+
+	write.table(b,paste0('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/B_SE_N/',protein,'.txt'),row.names = F,quote = F)
+	system(paste0('gzip ','/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/B_SE_N/',protein,'.txt'))
+
+}
+
+################################################################
+
+
+library(data.table)
+
+setwd('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/')
+
+filelist <- list.files('B_SE_N/',pattern = 'txt.gz')
+
+k <- 1
+
+sink(paste0('logs/b_se_check_n_',k,'.log'),split = TRUE)
+
+if (!dir.exists("/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/b_se/")){dir.create("/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/b_se/")}
+
+a <- fread(paste0('B_SE_N/',filelist[1]),data.table = FALSE)
+n <- a$N
+
+num <- length(filelist) - 1
+start_num <- num*(k-1)/5 + 1
+end_num <- num*k/5
+
+if (end_num == length(filelist) - 1) {
+	end_num <- end_num + 1
+}
+
+cat('processing ',start_num,' to ',end_num,' ... \n')
+
+for (i in start_num:end_num) {
+
+	cat('\n')
+	print(Sys.time())
+	protein <- sub(filelist[i],pattern='.txt.gz',replacement = '')
+	cat(i,'\t',protein,'\n')
+
+	if (file.exists(paste0('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/b_se/',protein,'.txt.gz'))) {
+		cat('Result file exist, next \n')
+		next
+	}
+
+	a <- fread(paste0('B_SE_N/',filelist[i]),data.table = FALSE)
+
+	if (all(a$N == n)) {
+
+		cat('n check:',all(a$N == n),'\n')
+		b <- a[,c('Beta','SE')]
+
+		write.table(b,paste0('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/b_se/',protein,'.txt'),row.names = F,quote = F)
+		system(paste0('gzip ','/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/b_se/',protein,'.txt'))
+		cat('write out done. \n')
+
+	} else {
+
+		cat('n check:',all(a$N == n),', next \n')
+		next
+
+
+	}
+	
+
+}
+
+sink()
+
+
+
+
---
+++ data_format_decode.R
@@ -0,0 +1,162 @@
+## data process decode
+## This script should be ran on LB
+
+# t1 <- Sys.time()
+# t2 <- Sys.time()
+# print(t2 - t1)
+
+## read input arguments
+args <- commandArgs(trailingOnly = TRUE)
+t <- as.numeric(args[1])
+start_num <- t*209 - 208
+end_num <- t*209
+
+
+setwd('/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/')
+sink(file = paste0('logs/process_decode_',start_num,'_',end_num,'.log'),split = TRUE)
+
+library(tidyverse)
+library(data.table)
+
+cat('################################################################','\n')
+print('MAF threshold 0.01',quote = F)
+cat('################################################################','\n')
+cat('\n')
+
+inputpath <- "/NiuHe/decode.is/"
+outputpath <- "/opt/storage/deCODE_formatted/zhijian/decode_somascan_data/"
+
+cis.info <- read.delim('/opt/share2Big/somascan_info/cis_info.txt',header = T, stringsAsFactors = F)
+cat('Reading annotated.txt.gz and excluded.txt.gz \n')
+cat('\n')
+annot <- read.table('/NiuHe/assocvariants.annotated.txt.gz',header = T,stringsAsFactors = F) # about 27 million SNPs
+exclude <- read.table('/NiuHe/assocvariants.excluded.txt.gz',header = T,stringsAsFactors = F) # about 3.7 million SNPs
+
+annot <- annot %>% select(-Chrom,-Pos) %>% filter(rsids != '.')
+# cis.info[cis.info$Chr == 'chrX',] <- 'chr23'
+# cis.info <- cis.info %>% mutate(Chr = as.numeric(sub(Chr,pattern = 'chr',replacement = '')))
+
+duplicatedSNP.annot <- annot$rsids[duplicated(annot$rsids)]
+# annot.lowMAF <- annot[annot$effectAlleleFreq == 0.0001 | annot$effectAlleleFreq == 0.9999,]
+# annot.lowMAF <- annot.lowMAF[!annot.lowMAF$rsids %in% exclude$rsids,]
+# annot.lowMAF <- annot.lowMAF[!annot.lowMAF$rsids %in% duplicatedSNP.annot,]
+# set.seed(911)
+# annot.lowMAF <- annot.lowMAF[sample.int(nrow(annot.lowMAF),2E4),]
+
+SNP_list_all <- SNP_list_intersect <- c()
+
+for (i in start_num:end_num) {
+
+	filename <- cis.info[i,'filename']
+	symbol <- paste(cis.info[i,'SeqId'],cis.info[i,'Gene'],sep = '_')
+	
+	print(Sys.time(),quote = F)
+	cat('\n')
+	cat(paste0(i,'  Processing ',symbol,'\n'))
+	cat(paste0('reading file ',filename,' ...','\n'))
+
+	t1 <- Sys.time()
+	a <- tryCatch(fread(file = paste0(inputpath,filename)),error = function(cond) {message(cond);return(cond)}) # about 30 million SNPs
+	a <- as.data.frame(a)
+	t2 <- Sys.time()
+	print(t2 - t1)
+
+	if (inherits(a,'error')) {
+		cat('\n')
+		cat('Failed to open file ',filename,'\n')
+		print(a)
+		next
+	}
+
+	#delete NA;delete duplicate
+	t1 <- Sys.time()
+	cat('Remove rsid is NA or in exculde \n')
+	A <- a %>% drop_na(rsids)  # about 26 million SNPs
+	A <- A %>% filter(!Name %in% exclude$Name) %>% inner_join(.,annot,by = c('rsids','effectAllele'))# about 23 million SNPs
+	A <- A %>% mutate(Z = Beta/SE) %>% select(,c(-minus_log10_pval,-otherAllele.y,-Name.y))
+	colnames(A) <- sub(colnames(A),pattern='.x',replacement='')
+	A$Z <- round(A$Z,digits = 6) 
+
+	t2 <- Sys.time()
+	print(t2 - t1)
+	
+
+	# A[A$Chrom == 'chrX','Chrom'] <- 'chr23'
+	# A <- A %>% mutate(Chrom = as.numeric(sub(Chrom,pattern = 'chr',replacement = '')))
+
+	t1 <- Sys.time()
+
+	cat('Saving cis region ... \t')
+	cis.chr <- cis.info$Chr[i]
+	A.chr <- A[A$Chrom == cis.chr,]
+	TSS.pos <- cis.info$TSS[i]
+	# 1Mb window centred top cis-pQTL
+	cis <- A.chr %>% filter(Pos < TSS.pos + 5E5 & Pos > TSS.pos - 5E5)
+	write.table(cis,file = paste0(outputpath,'cis_1Mb/',symbol,'_cis.txt'),row.names = F,quote = F,sep = '\t')
+
+	top.idx <- which.max(abs(A$Z))
+	top.rsid <- A[top.idx,'rsids']
+	if (!top.rsid %in% cis$rsids) {
+
+		cat('Top SNP is not located on cis-region.\n')
+		cat('Saving top SNP region.\n')
+
+		top.chr <- A[top.idx,'Chrom']
+		A.chr <- A[A$Chrom == top.chr,]
+		top.pos <- A[top.idx,'Pos']
+		top <- A.chr %>% filter(Pos < top.pos + 5E5 & Pos > top.pos - 5E5)
+		write.table(top,file = paste0(outputpath,'top_1Mb/',symbol,'_top.txt'),row.names = F,quote = F,sep = '\t')
+
+	}
+	
+
+	A <- A %>% distinct(rsids,.keep_all = TRUE)
+
+	# cat('Saving 2E4 lowmaf SNPs ... \n')
+	# lowmaf.idx <- order(A$ImpMAF)[1:2E4]
+	# lowmaf <- data.frame(rsids = A[lowmaf.idx,'rsids'],Z = A[lowmaf.idx,'Z'],stringsAsFactors = F)
+
+	cat('Saving lowmaf SNPs (MAF = 0.0001) ... \n')
+	lowmaf <- A[A$ImpMAF == 0.0001,c('rsids','Z')]
+	lowmaf <- lowmaf[!lowmaf$rsids %in% duplicatedSNP.annot,]
+
+	write.table(lowmaf,file = paste0(outputpath,'lowmaf/',symbol,'_lowmaf.txt'),quote = F,row.names = F,sep = '\t')
+
+	t2 <- Sys.time()
+	print(t2 - t1)
+	
+	A1 <- A %>% filter(ImpMAF > 0.01) # about 10 million SNPs
+	
+	snplist <- A1[,c('rsids','Chrom','Pos','effectAllele','otherAllele','effectAlleleFreq')]
+	if (is.null(SNP_list_all)) {
+		SNP_list_all <- snplist
+		SNP_list_intersect <- snplist
+	} else {
+		SNP_list_all <- full_join(SNP_list_all,snplist,by = c('rsids','Chrom','Pos','effectAllele','otherAllele','effectAlleleFreq'))
+		SNP_list_intersect <- inner_join(SNP_list_intersect,snplist,by = c('rsids','Chrom','Pos','effectAllele','otherAllele','effectAlleleFreq'))
+	}
+	
+
+	A1 <- A1 %>% dplyr::select(-Name,-Chrom,-Pos,-Pval,-Z,-ImpMAF) # 0.986Gb in R
+	
+	cat(paste0(nrow(A1),' SNPs were written in file',symbol,'.txt','\n'))
+	# saveRDS(A1,file = paste0(outputpath,'RDS/',symbol,'.rds')) # ~ 174M
+	fwrite(A1,file = paste0(outputpath,'sumstat/',symbol,'.txt'),quote = FALSE,row.names = FALSE,sep = '\t')
+	system(paste0('gzip ',outputpath,'sumstat/',symbol,'.txt'))
+
+	A1 <- A1 %>% dplyr::select(rsids,Beta,SE)
+	# saveRDS(A1,file = paste0(outputpath,'B_SE/',symbol,'.rds'))
+	fwrite(A1,file = paste0(outputpath,'B_SE/',symbol,'.txt'),quote = FALSE,row.names = FALSE,sep = '\t')
+	system(paste0('gzip ',outputpath,'B_SE/',symbol,'.txt'))
+
+	cat('\n')
+
+	gc()
+}
+
+cat('The whole SNP list saved in SNP_list_all.rds and SNPs shared by all protein in SNP_list_intersect.rds \n')
+saveRDS(SNP_list_all,file = paste0(outputpath,'SNP_list/SNP_list_all_',start_num,'_',end_num,'.rds'))
+saveRDS(SNP_list_intersect,file = paste0(outputpath,'SNP_list/SNP_list_intersect_',start_num,'_',end_num,'.rds'))
+
+sink()
+
